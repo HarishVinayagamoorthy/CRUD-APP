0d3f4773a7e9a386069e348afaeecf5e7e0ce8b6
HarishVinayagamoorthy <harishvinayagamoorthy123@gmail.com>
2023-09-15 13:28:57 +0530

    Task completed

---
+++ script.js
@@ -0,0 +1,72 @@
+//!initial values
+var selectedRow = null;
+//!form submit logic
+function onFormSubmit(e) {
+  event.preventDefault();
+  var formData = readFormData();
+  if (selectedRow == null) {
+    insertNewRecord(formData);
+  } else {
+    updateRecord(formData);
+  }
+  resetForm();
+}
+//!get method(Retriving the data)
+function readFormData() {
+  var formData = {};
+  formData["productCode"] = document.getElementById("productCode").value;
+  formData["product"] = document.getElementById("product").value;
+  formData["qty"] = document.getElementById("qty").value;
+  formData["perPrice"] = document.getElementById("perPrice").value;
+  return formData;
+}
+//!insert the data (Post method)
+function insertNewRecord(data) {
+  var table = document
+    .getElementById("storeList")
+    .getElementsByTagName("tbody")[0];
+  var newRow = table.insertRow(table.length);
+  cell1 = newRow.insertCell(0);
+  cell1.innerHTML = data.productCode;
+  cell2 = newRow.insertCell(1);
+  cell2.innerHTML = data.product;
+  cell3 = newRow.insertCell(2);
+  cell3.innerHTML = data.qty;
+  cell4 = newRow.insertCell(3);
+  cell4.innerHTML = data.perPrice;
+  cell4 = newRow.insertCell(4);
+  cell4.innerHTML = `<button onClick="onEdit(this)">Edit</button> <button onClick = "onDelete(this)">Delete</button>`;
+}
+//!edit and update the data (Update method)
+//editing the data(get)
+function onEdit(td) {
+  selectedRow = td.parentElement.parentElement;
+  document.getElementById("productCode").value = selectedRow.cells[0].innerHTML;
+  document.getElementById("product").value = selectedRow.cells[1].innerHTML;
+  document.getElementById("qty").value = selectedRow.cells[2].innerHTML;
+  document.getElementById("perPrice").value = selectedRow.cells[3].innerHTML;
+}
+//updating the data
+function updateRecord(formData) {
+  selectedRow.cells[0].innerHTML = formData.productCode;
+  selectedRow.cells[1].innerHTML = formData.product;
+  selectedRow.cells[2].innerHTML = formData.qty;
+  selectedRow.cells[3].innerHTML = formData.perPrice;
+}
+//!deleting the data (delete method)
+//delete the data
+function onDelete(td) {
+  if (confirm("Are you sure about deleting😒 the data ?")) {
+    row = td.parentElement.parentElement;
+    document.getElementById("storeList").deleteRow(row.rowIndex);
+    resetForm();
+  }
+}
+//!reseting the values in form
+function resetForm() {
+  document.getElementById("productCode").value = "";
+  document.getElementById("product").value = "";
+  document.getElementById("qty").value = "";
+  document.getElementById("perPrice").value = "";
+  selectedRow = null;
+}
